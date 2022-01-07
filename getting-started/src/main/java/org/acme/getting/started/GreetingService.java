128fb26147f2a580921597b48b6ea0d5d08ec31e
Damiano Fasoli <damiano.fasoli@gmail.com>
2022-01-07 09:40:54 +0100

    .

--- getting-started/src/main/java/org/acme/getting/started/GreetingService.java
+++ getting-started/src/main/java/org/acme/getting/started/GreetingService.java
@@ -6,7 +6,7 @@ import javax.enterprise.context.ApplicationScoped;
 public class GreetingService {
 
     public String greeting(String name) {
-        return "hello there " + name;
+        return "hello there1 " + name;
     }
 
 }
